3b0959e8c5b43cf079429619d7629f5a0f34cdba
Prasetya36 <ariefprasetya1234@gmail.com>
2023-11-02 11:56:34 +0700

    first

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:maps_app/Widget/User.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MapSample(),
+    );
+  }
+}
+
+class MapSample extends StatefulWidget {
+  const MapSample({super.key});
+
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<MapSample> {
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(-6.7287963889926115, 108.54707757971818),
+    zoom: 14.4746,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children:[
+          GoogleMap(
+          mapType: MapType.terrain,
+          initialCameraPosition: _kGooglePlex,
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          Marker(
+  markerId: const MarkerId("marker1"),
+  position: const LatLng(-6.7287963889926115, 108.54707757971818),
+  draggable: true,
+  onDragEnd: (value) {
+
+  },
+);
+          },
+        ),
+        Stack(children: [ 
+          Container(
+            alignment: Alignment.center,
+            padding: EdgeInsets.only(top: 500),
+            child: User(),)
+          
+        ],)
+        ] 
+      ),
+      
+    );
+  }
+}
